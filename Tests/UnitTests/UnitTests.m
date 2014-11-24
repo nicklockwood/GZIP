a35b4a1c76af681e1fc80a5cb77c5883724a38c0
Anders Östlin <anders.ostlin@apegroup.com>
2014-11-24 14:53:35 +0100

    Add simple performance tests

--- Tests/UnitTests/UnitTests.m
+++ Tests/UnitTests/UnitTests.m
@@ -9,6 +9,19 @@
 #import <XCTest/XCTest.h>
 #import "GZIP.h"
 
+#include <stdlib.h>
+
+static NSData* createRandomNSData()
+{
+    NSUInteger size = 10*1024*1024;
+    NSUInteger chunkLenght = 4;
+    NSMutableData *data = [NSMutableData dataWithCapacity:size];
+    for (NSUInteger index = 0; index < size/chunkLenght; index++ ) {
+        u_int32_t randomBits = arc4random();
+        [data appendBytes:(void*)&randomBits length:chunkLenght];
+    }
+    return data;
+}
 
 @interface UnitTests : XCTestCase
 
@@ -39,6 +52,23 @@
     
     data = [[NSData data] gunzippedData];
     XCTAssertNil(data, @"ZeroLengthInput test failed");
+}
+
+- (void)testCompressionPerformance
+{
+    NSData *inputData = createRandomNSData();
+    [self measureBlock:^{
+        __unused NSData *compressedData = [inputData gzippedData];
+    }];
+}
+
+- (void)testDecompressionPerformance
+{
+    NSData *inputData = createRandomNSData();
+    NSData *compressedData = [inputData gzippedData];
+    [self measureBlock:^{
+        __unused NSData *outputData = [compressedData gunzippedData];
+    }];
 }
 
 @end
