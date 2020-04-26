c9b18a0fda2e9f40b72bff2aee7bdb2d2bcd23c8
Simon Støvring <mail@simonbs.dk>
2020-04-26 15:50:43 +0200

    Adds support for Swift Package Manager (#36)

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GZIP",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "GZIP",
+            targets: ["GZIP"]),
+    ],
+    targets: [
+        .target(
+            name: "GZIP",
+            path: "GZIP",
+            sources: ["Sources"],
+            publicHeadersPath: "Sources")
+    ]
+)
